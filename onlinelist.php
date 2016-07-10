d17d49cf93dea2c29e1738cf62b6a1d73f6dbad5
Robsoon <robsonportnoy@hotmail.com>
2016-07-10 01:17:00 -0300

    Update onlinelist.php
    Adicionada a table Flags que mostra a quantidade de players online por país.

--- onlinelist.php
+++ onlinelist.php
@@ -2,11 +2,27 @@
 
 <h1>Who is online?</h1>
 <?php
-$array = online_list();
+$OnlineList = online_list();
+$OnlineListPerFlag = online_list_per_flag();
 
-
-if ($array) {
+if ($OnlineList) {
 	?>
+	 
+	<table id="Flags" class="table table-striped table-hover">
+		<tr class="yellow">
+			<th></th>
+			<th>Online:</th>
+		</tr>
+		<?php
+			foreach ($OnlineListPerFlag as $value) {
+				echo "<tr>";
+				echo "	<td> <img src='\flags\\'" . $value['flag'].".png> </td>";
+				echo "  <td>".$value['pais']."</td>";
+				echo "</tr>";
+			}
+		?>
+	</table>
+	
 	
 	<table id="onlinelistTable" class="table table-striped table-hover">
 		<tr class="yellow">
@@ -21,20 +37,20 @@ if ($array) {
 			<th>Vocation:</th>
 		</tr>
 			<?php
-			foreach ($array as $value) {
-			$account_data = user_znote_account_data($value['account_id'], 'flag');
-			$url = url("characterprofile.php?name=". $value['name']);
-			$resets = preg_replace("/[^0-9]/","",$value['description']);
-			
-			echo '<tr class="special" onclick="javascript:window.location.href=\'' . $url . '\'">';
-			if ($config['country_flags'])
-				echo '<td><img src="\flags\\' . $account_data['flag'] . '.png"></td>';			
-			echo '<td><a href="characterprofile.php?name='. $value['name'] .'">'. $value['name'] .'</a></td>';
-			if (!empty($value['gname'])) echo '<td><a href="guilds.php?name='. $value['gname'] .'">'. $value['gname'] .'</a></td>'; else echo '<td></td>'; 
-			echo '<td>'.$resets.'</td>';
-			echo '<td>'. $value['level'] .'</td>';
-			echo '<td>'. vocation_id_to_name($value['vocation']) .'</td>';
-			echo '</tr>';
+			foreach ($OnlineList as $value) {
+				$account_data = user_znote_account_data($value['account_id'], 'flag');
+				$url = url("characterprofile.php?name=". $value['name']);
+				$resets = preg_replace("/[^0-9]/","",$value['description']);
+				
+				echo '<tr class="special" onclick="javascript:window.location.href=\'' . $url . '\'">';
+				if ($config['country_flags'])
+					echo '<td><img src="\flags\\' . $account_data['flag'] . '.png"></td>';			
+				echo '<td><a href="characterprofile.php?name='. $value['name'] .'">'. $value['name'] .'</a></td>';
+				if (!empty($value['gname'])) echo '<td><a href="guilds.php?name='. $value['gname'] .'">'. $value['gname'] .'</a></td>'; else echo '<td></td>'; 
+				echo '<td>'.$resets.'</td>';
+				echo '<td>'. $value['level'] .'</td>';
+				echo '<td>'. vocation_id_to_name($value['vocation']) .'</td>';
+				echo '</tr>';
 			}
 			?>
 	</table>
